48f24cfc259440d2a371f1086354086911ac8c98
Br3nd4nt <Vlad.belov.08@gmail.com>
2025-08-23 00:22:52 +0300

    feat: added models and changed the data creation script

---
+++ F1Vision/Core/Models/Race/TyreCompound.swift
@@ -0,0 +1,15 @@
+//
+//  TyreCompound.swift
+//  F1Vision
+//
+//  Created by br3nd4nt on 22.08.2025.
+//
+
+enum TyreCompound: String, Codable {
+    case soft
+    case medium
+    case hard
+    case intermediate
+    case wet
+    case unknown
+}
