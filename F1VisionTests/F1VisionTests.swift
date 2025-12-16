a2e1d6966db95081441114d6e192ab9e028c1042
Br3nd4nt <Vlad.belov.08@gmail.com>
2025-12-16 03:36:32 +0300

    feat: added new target and swift-testing

---
+++ F1VisionTests/F1VisionTests.swift
@@ -0,0 +1,16 @@
+//
+//  F1VisionTests.swift
+//  F1VisionTests
+//
+//  Created by br3nd4nt on 16.12.2025.
+//
+
+import Testing
+
+struct F1VisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
